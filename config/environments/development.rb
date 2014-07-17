f269d4b5f1f2ccf9cc607c0ed254be2a597e4c2b
Norm <nalcorp1@gmail.com>
2014-07-17 01:43:17 -0700

    back to normal

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-etsydemo::Application.configure do
+Etsydemo::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
